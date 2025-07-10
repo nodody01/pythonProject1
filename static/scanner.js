8190338ac6e3f326dd13c1d531db5dfbfef298de
nodody01 <kirill89629731141@gmail.com>
2025-07-10 11:10:18 +0300

    fdf

--- static/scanner.js
+++ static/scanner.js
@@ -7,7 +7,7 @@ Quagga.init({
         },
     },
     decoder: {
-        readers: ["qr_code_reader"]
+        readers: ["code_128_reader", "ean_reader", "upc_reader", "qr_code_reader"]
     }
 }, function(err) {
     if (err) {
@@ -36,27 +36,27 @@ Quagga.init({
 //};
 
 
-Quagga.onDetected(function (data) {
-    const uuid = data.codeResult.code;
-
-    // Показываем результат
-    const resultEl = document.getElementById('result');
-    resultEl.textContent = 'Сканирование...';
-
-    // Отправляем UUID на сервер
-    fetch('/api/scan', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ uuid })
-    })
-    .then(res => res.json())
-    .then(json => {
-        resultEl.textContent = json.message;
-    })
-    .catch(err => {
-        console.error(err);
-        resultEl.textContent = 'Ошибка при обработке QR-кода';
-    });
-});
+//Quagga.onDetected(function (data) {
+//    const uuid = data.codeResult.code;
+//
+//    // Показываем результат
+//    const resultEl = document.getElementById('result');
+//    resultEl.textContent = 'Сканирование...';
+//
+//    // Отправляем UUID на сервер
+//    fetch('/api/scan', {
+//        method: 'POST',
+//        headers: {
+//            'Content-Type': 'application/json'
+//        },
+//        body: JSON.stringify({ uuid })
+//    })
+//    .then(res => res.json())
+//    .then(json => {
+//        resultEl.textContent = json.message;
+//    })
+//    .catch(err => {
+//        console.error(err);
+//        resultEl.textContent = 'Ошибка при обработке QR-кода';
+//    });
+//});
